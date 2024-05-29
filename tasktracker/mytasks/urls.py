a1b503d7e88c11affee8ca77d87de3442c70e4b8
Reigns360 <triciareigns360@gmail.com>
2024-05-29 03:15:11 -0700

    initial commit

---
+++ tasktracker/mytasks/urls.py
@@ -0,0 +1,11 @@
+# from django.urls import path, include
+# from . import views
+# from .views import *
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from rest_framework.routers import DefaultRouter 
+# from .views import TaskViewSet
+
+# router = DefaultRouter()
+# router.register(r'tasks', TaskViewSet)
+
